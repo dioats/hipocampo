f57bf35d246002de1e5a9c9bab9dde04625cd3d4
Diogo Ataides <dioataides@gmail.com>
2024-10-20 16:29:26 -0300

    feat: main and access screens

--- index.js
+++ index.js
@@ -10,17 +10,42 @@ app.use(express.static("public"));
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set('views', path.resolve(__dirname, "./views"));
+app.use(express.urlencoded())
 
 app.get('/', function (req, res) {
-  res.render("home");
+
+  const isLogged = false;
+
+  if(isLogged) {
+    return res.render("home");
+  }
+  
+  return res.render("main", {css: ["main.css"]});
 });
 
-app.get('/register', function (req, res){
-  res.render("register");
+app.get('/register', function (req, res) {
+  res.render("register", {css: ["access.css"]});
 });
 
-app.get('/login', function (req, res){
-  res.render("login");
+app.post('/register', function (req, res) {
+  console.log(req.body)
+  res.render("register", {css: ["access.css"]});
+});
+
+app.get('/login', function (req, res) {
+  res.render("login", {css: ["access.css"]});
+});
+
+app.get('/profile', function (req, res) {
+  res.render("profile");
+});
+
+app.get('/note/new', function (req, res) {
+  res.render("note");
+});
+
+app.get('/note/:id', function (req, res) {
+  res.render("note");
 });
 
 const PORT = process.env.PORT || 3000;
